98fef73a96b4e22cb67f4a1b7ab32ea35a7c630a
엘풍 <eruhoon@gmail.com>
2023-12-17 01:47:55 +0900

    update whitelist

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import { MapleInvenLoader } from './loader/MapleInvenLoader';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (typeof origin === 'string' && isWhiteList(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -16,3 +25,8 @@ app.get('/maple-inven', async (_, res) => {
   const results = await new MapleInvenLoader().load();
   res.send(results);
 });
+
+function isWhiteList(host: string): boolean {
+  const whiteList = ['https://eruhoon.github.io', 'http://localhost:5173'];
+  return whiteList.indexOf(host) > -1;
+}
