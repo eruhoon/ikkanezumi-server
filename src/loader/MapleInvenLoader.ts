2aa28c811304a1d6c9a8c150c6d5c76e81db2f02
엘풍 <eruhoon@gmail.com>
2024-01-03 22:35:15 +0900

    optimize code

--- src/loader/MapleInvenLoader.ts
+++ src/loader/MapleInvenLoader.ts
@@ -3,9 +3,8 @@ import * as cheerio from 'cheerio';
 
 export class MapleInvenLoader {
   async load(): Promise<MapleInvenLoaderResult[]> {
-    const url = 'https://www.inven.co.kr/board/maple/5974';
-    const url2 = 'https://www.inven.co.kr/board/maple/5974?my=chuchu&vtype=pc'; 
-    const { data } = await axios.get<string>(url2);
+    const url = 'https://www.inven.co.kr/board/maple/5974?my=chuchu&vtype=pc';
+    const { data } = await axios.get<string>(url);
 
     const $ = cheerio.load(data);
     const $board = $('article#new-board div.board-list');
